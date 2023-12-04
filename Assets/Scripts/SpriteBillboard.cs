bffc84a3842b316f0a91f1cb697d5ac9a1973930
Myan <118029565+MaximTomato@users.noreply.github.com>
2023-12-04 05:50:54 -0500

    Billboarding fixed?
    So I got the billboarding to work, but it's for the editor's camera and not the game camera.
    
    In the SpriteBillboarding script, I used camera.current, which gave this weird situation. But camera.main doesn't work, so idk.

--- Assets/Scripts/SpriteBillboard.cs
+++ Assets/Scripts/SpriteBillboard.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class SpriteBillboard : MonoBehaviour
 {
     [SerializeField] bool freezeXZAxis = true;
-    public System.Random ran = new System.Random();
+    //public System.Random ran = new System.Random();
 
     // Update is called once per frame
     void Update()
     {
         if (freezeXZAxis)
         {
-            transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+            transform.rotation = Quaternion.Euler(0f, Camera.current.transform.rotation.eulerAngles.y, 0f);
         }
         else
         {
-            transform.rotation = Camera.main.transform.rotation;
+            transform.rotation = Camera.current.transform.rotation;
             //transform.rotation = Quaternion.Euler(ran.Next(0, 90), 90f, 0f);
         }
-        print("Rotation is working "+ Camera.main.transform.rotation.eulerAngles.y);
+        print("Rotation is working "+ Camera.current.transform.rotation.eulerAngles.y);
     }
 }
